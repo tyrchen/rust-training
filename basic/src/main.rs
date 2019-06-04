af1b1bad4330c21d7fc08ca81b1e317396e9ee80
Tyr Chen <tyr.chen@gmail.com>
2019-06-03 23:32:27 -0700

    add lecture 1 code

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-  println!("Hello, basic grammar!");
+  // previous owner
+  let age = 37u128; // copy / clone trait
+  let first = String::from("Tyr"); // type inference "Tyr" => string section 0x32756000
+  let last = String::from("Chen");
+  let result = {
+    let fullname = full_name(&first, &last);
+    &fullname
+  };
+  let languages = vec!["elixir", "python", "js", "rust"];
+  println!("Hello, {} with age {}, knows {:?}!", first, age, languages); // borrow again
 }
+
+fn full_name(first: &String, last: &String) -> String {
+  format!("{} {}", first, last)
+} // first / last be freed
