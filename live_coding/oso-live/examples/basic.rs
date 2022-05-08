5e7a25242b6fe521cd27db5c8336f1816dbd078e
Tyr Chen <tyr.chen@gmail.com>
2022-05-07 17:13:42 -0700

    live coding for building a permission data structure

--- live_coding/oso-live/examples/basic.rs
+++ live_coding/oso-live/examples/basic.rs
@@ -1,67 +1,8 @@
 use anyhow::Result;
-use oso::{Oso, PolarClass};
-
-#[allow(dead_code)]
-#[derive(Clone, PolarClass)]
-struct User {
-    pub email: String,
-    pub full_name: String,
-    #[polar(attribute)]
-    pub repo_roles: Vec<RepoRole>,
-}
-
-#[derive(Clone, PolarClass)]
-struct Repo {
-    #[polar(attribute)]
-    pub name: String,
-    #[polar(attribute)]
-    pub public: bool,
-}
-
-#[derive(Clone, PolarClass)]
-struct RepoRole {
-    #[polar(attribute)]
-    pub name: String,
-    #[polar(attribute)]
-    pub repo: String,
-}
-
-impl User {
-    pub fn new(email: impl Into<String>, full_name: impl Into<String>) -> User {
-        User {
-            email: email.into(),
-            full_name: full_name.into(),
-            repo_roles: Vec::new(),
-        }
-    }
-
-    pub fn add_role(&mut self, repo: impl Into<String>, role: impl Into<String>) {
-        self.repo_roles.push(RepoRole {
-            name: role.into(),
-            repo: repo.into(),
-        });
-    }
-}
-
-impl Repo {
-    pub fn new(name: impl Into<String>, public: bool) -> Repo {
-        Repo {
-            name: name.into(),
-            public,
-        }
-    }
-}
+use oso_live::{Permission, Repo, RepoRole, User};
 
 fn main() -> Result<()> {
-    let mut oso = Oso::new();
-    oso.register_class(User::get_polar_class_builder().build())?;
-    oso.register_class(Repo::get_polar_class_builder().build())?;
-    oso.register_class(RepoRole::get_polar_class_builder().build())?;
-
-    oso.load_files(vec![format!(
-        "{}/examples/app.polar",
-        env!("CARGO_MANIFEST_DIR")
-    )])?;
+    let perm = Permission::<User, RepoRole, Repo>::try_new().unwrap();
 
     let rust_repo = Repo::new("acme/rust", false);
     let oso_repo = Repo::new("osohq/oso", true);
@@ -74,26 +15,26 @@ fn main() -> Result<()> {
 
     println!(
         "tyr allowed delete: {}", // no
-        oso.is_allowed(tyr.clone(), "delete", rust_repo.clone())?
+        perm.is_allowed(tyr.clone(), "delete", rust_repo.clone())?
     );
     println!(
         "tyr allowed write: {}", // yes
-        oso.is_allowed(tyr.clone(), "write", rust_repo.clone())?
+        perm.is_allowed(tyr.clone(), "write", rust_repo.clone())?
     );
 
     println!(
         "alice allowed write: {}", // no
-        oso.is_allowed(alice.clone(), "write", rust_repo.clone())?
+        perm.is_allowed(alice.clone(), "write", rust_repo.clone())?
     );
 
     println!(
-        "alice allowed billing: {}", // no
-        oso.is_allowed(alice, "billing", rust_repo)?
+        "alice allowed billing: {}", // yes
+        perm.is_allowed(alice, "billing", rust_repo)?
     );
 
     println!(
-        "tyr allowed read oso: {}", // no
-        oso.is_allowed(tyr, "read", oso_repo)?
+        "tyr allowed read oso: {}", // yes
+        perm.is_allowed(tyr, "read", oso_repo)?
     );
     Ok(())
 }
--- live_coding/oso-live/src/lib.rs
+++ live_coding/oso-live/src/lib.rs
@@ -1,8 +1,133 @@
+use oso::{FromPolar, Oso, OsoError, PolarClass, ToPolar};
+use std::{collections::HashSet, env, fs, hash::Hash, marker::PhantomData, sync::Arc};
+
+#[allow(dead_code)]
+#[derive(Clone, PolarClass)]
+pub struct User {
+    pub email: String,
+    pub full_name: String,
+    #[polar(attribute)]
+    pub repo_roles: Vec<RepoRole>,
+}
+
+#[derive(Clone, PolarClass)]
+pub struct Repo {
+    #[polar(attribute)]
+    pub name: String,
+    #[polar(attribute)]
+    pub public: bool,
+}
+
+#[derive(Clone, PolarClass)]
+pub struct RepoRole {
+    #[polar(attribute)]
+    pub name: String,
+    #[polar(attribute)]
+    pub repo: String,
+}
+
+impl User {
+    pub fn new(email: impl Into<String>, full_name: impl Into<String>) -> User {
+        User {
+            email: email.into(),
+            full_name: full_name.into(),
+            repo_roles: Vec::new(),
+        }
+    }
+
+    pub fn add_role(&mut self, repo: impl Into<String>, role: impl Into<String>) {
+        self.repo_roles.push(RepoRole {
+            name: role.into(),
+            repo: repo.into(),
+        });
+    }
+}
+
+impl Repo {
+    pub fn new(name: impl Into<String>, public: bool) -> Repo {
+        Repo {
+            name: name.into(),
+            public,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Permission<U, UR, R> {
+    oso: Arc<Oso>,
+    _user: PhantomData<U>,
+    _user_role: PhantomData<UR>,
+    _resource: PhantomData<R>,
+}
+
+impl<U, UR, R> Permission<U, UR, R>
+where
+    U: PolarClass + ToPolar,
+    UR: PolarClass,
+    R: PolarClass + ToPolar,
+{
+    pub fn try_new() -> Result<Self, OsoError> {
+        let mut oso = Oso::new();
+        oso.register_class(U::get_polar_class_builder().build())?;
+        oso.register_class(UR::get_polar_class_builder().build())?;
+        oso.register_class(R::get_polar_class_builder().build())?;
+
+        Permission::<U, UR, R>::load_policies(&mut oso)?;
+
+        Ok(Permission {
+            oso: Arc::new(oso),
+            _user: PhantomData,
+            _user_role: PhantomData,
+            _resource: PhantomData,
+        })
+    }
+
+    pub fn is_allowed<A>(&self, actor: U, action: A, resource: R) -> Result<bool, OsoError>
+    where
+        A: ToPolar,
+    {
+        self.oso.is_allowed(actor, action, resource)
+    }
+
+    pub fn get_allowed_actions<A>(&self, actor: U, resource: R) -> Result<HashSet<A>, OsoError>
+    where
+        A: FromPolar + Eq + Hash,
+    {
+        self.oso.get_allowed_actions(actor, resource)
+    }
+
+    pub fn get_roles(&self, _actor: U, _resource: R) -> Result<HashSet<UR>, OsoError> {
+        todo!()
+    }
+
+    fn load_policies(oso: &mut Oso) -> Result<(), OsoError> {
+        let base_dir = env::var("POLICY_DIR")
+            .unwrap_or_else(|_| format!("{}/rules", env!("CARGO_MANIFEST_DIR")));
+
+        let files = fs::read_dir(base_dir)?
+            .filter_map(|entry| entry.ok())
+            .map(|entry| entry.path())
+            .filter(|p| p.is_file() && p.extension() == Some(std::ffi::OsStr::new("polar")))
+            .collect::<Vec<_>>();
+        oso.load_files(files)?;
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn permission_should_work() {
+        let perm = Permission::<User, RepoRole, Repo>::try_new().unwrap();
+        let mut user = User::new("tyr@acme.com", "Tyr");
+        let repo = Repo::new("permission", false);
+        user.add_role(&repo.name, "writer");
+        assert!(perm.is_allowed(user.clone(), "read", repo.clone()).unwrap());
+        let actions: HashSet<String> = perm.get_allowed_actions(user, repo).unwrap();
+        assert_eq!(
+            &actions,
+            &["read".into(), "write".into()].into_iter().collect()
+        );
     }
 }
